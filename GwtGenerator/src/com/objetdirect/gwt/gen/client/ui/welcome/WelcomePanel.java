1380a1409d68b53b1a98a16072014b42b54491ed
raphael.brugier <raphael.brugier@gmail.com>
2010-05-19 16:54:09 +0000

    Replace the dockLayoutPanel by a spliLayoutPanel between the diagramsList and the content panel.

--- GwtGenerator/src/com/objetdirect/gwt/gen/client/ui/welcome/WelcomePanel.java
+++ GwtGenerator/src/com/objetdirect/gwt/gen/client/ui/welcome/WelcomePanel.java
@@ -24,6 +24,7 @@ import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.InlineHyperlink;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.objetdirect.gwt.gen.client.event.DesignAsAGuestEvent;
 
