ea4f4657712d8818f8011c2925ebb04c64bfdad7
raphael.brugier@gmail.com <raphael.brugier@gmail.com@4ee531d9-a8a3-626b-ffc3-51befb65a146>
2010-07-21 09:16:16 +0000

    An object diagram is not enhanced dynamically when getting with the seam classes and relations supported by the generator. This classes and relations are get from the special seam class diagram created by an administrator.

--- trunk/GwtGenerator/src/com/objetdirect/gwt/gen/server/helpers/ObjectDiagramBuilder.java
+++ trunk/GwtGenerator/src/com/objetdirect/gwt/gen/server/helpers/ObjectDiagramBuilder.java
@@ -15,40 +15,52 @@
 package com.objetdirect.gwt.gen.server.helpers;
 
 import static com.objetdirect.gwt.umlapi.client.umlcomponents.DiagramType.OBJECT;
-import static com.objetdirect.gwt.umlapi.client.umlcomponents.UMLVisibility.PRIVATE;
 import static com.objetdirect.gwt.umlapi.client.umlcomponents.umlrelation.UMLRelation.createAssociation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.objetdirect.gwt.gen.server.dao.DiagramDao;
+import com.objetdirect.gwt.gen.server.dao.SeamDiagramDao;
+import com.objetdirect.gwt.gen.server.entities.SeamDiagram;
 import com.objetdirect.gwt.gen.shared.dto.DiagramDto;
 import com.objetdirect.gwt.umlapi.client.umlCanvas.ClassDiagram;
 import com.objetdirect.gwt.umlapi.client.umlCanvas.ObjectDiagram;
+import com.objetdirect.gwt.umlapi.client.umlCanvas.UMLCanvasClassDiagram;
 import com.objetdirect.gwt.umlapi.client.umlcomponents.UMLClass;
 import com.objetdirect.gwt.umlapi.client.umlcomponents.umlrelation.UMLRelation;
 
 /**
- * This class is responsible to add the classes and class relations to an object diagram.
+ * This class is responsible to add the classes and class relations to an object diagram from the class diagram instantiated.
+ * It also adds the seam classes and relation supported by the generator.
  * 
  * @author Raphaël Brugier <raphael dot brugier at gmail dot com>
  */
 public class ObjectDiagramBuilder {
 
+	private static final String ENTITY_CLASS_NAME = "Entity";
+
 	private final DiagramDao diagramDao;
 	
 	private final DiagramDto objectDiagramDto;
 	
+	private final SeamDiagramDao seamDiagramDao;
+	
 	/**
-	 * @param objectDiagram
-	 * @param diagramDao
+	 * Construct a builder.
+	 * 
+	 * @param objectDiagramDto the diagram which need to be enhanced
+	 * @param diagramDao dao to access to the diagrams
+	 * @param seamDiagramDao dao to access to the seam diagram
 	 */
-	public ObjectDiagramBuilder(DiagramDto objectDiagramDto, DiagramDao diagramDao) {
+	public ObjectDiagramBuilder(DiagramDto objectDiagramDto, DiagramDao diagramDao, SeamDiagramDao seamDiagramDao) {
 		this.diagramDao = diagramDao;
 		this.objectDiagramDto = objectDiagramDto;
+		this.seamDiagramDao = seamDiagramDao;
 	}
 
 	/**
-	 * @return
+	 * @return the diagramDto enhanced with the classes and relations supported by the generator.
 	 */
 	public DiagramDto getObjectDiagramTransformed() {
 		addClassDiagramToObjectDiagram();
@@ -74,54 +86,73 @@ public class ObjectDiagramBuilder {
 		
 		List<UMLClass> domainClasses = classDiagram.getUmlClasses();
 		List<UMLRelation> classRelations = classDiagram.getClassRelations();
+		
 		addSeamClasses(domainClasses, classRelations);
+		
 		objectDiagram.setClasses(domainClasses);
 		objectDiagram.setClassRelations(classRelations);
 	}
 	
 	/**
-	 * Hard coded method to add the seam classes to the list of instantiable classes.
+	 * Add the seam classes and relations to the given classes and relations.
+	 * The seam classes and relations are found in the special seam diagram.
 	 */
 	private void addSeamClasses(final List<UMLClass> domainClasses, final List<UMLRelation> classRelations) {
-		UMLClass printDescriptorClass =  new UMLClass("PrintDescriptor").
-			addAttribute(PRIVATE, "String", "classPackageName").
-			addAttribute(PRIVATE, "String", "className").
-			addAttribute(PRIVATE, "String", "viewPackageName").
-			addAttribute(PRIVATE, "String", "viewName");
+
+		SeamDiagram seamDiagram = seamDiagramDao.getSeamDiagram();
+		UMLCanvasClassDiagram seamClassDiagram = (UMLCanvasClassDiagram) seamDiagram.getCanvas();
 		
-		UMLClass printEntityClass =  new UMLClass("PrintEntity");
+		List<UMLClass> seamSupportedClasses = createSeamSupportedClasses(seamClassDiagram.getUmlClasses());
 		
-		UMLRelation featureRelation = createAssociation(printDescriptorClass, printEntityClass, "feature");
-		classRelations.add(featureRelation);
+		List<UMLRelation> seamRelations = seamClassDiagram.getClassRelations();
 		
-		UMLClass printFormClass =  new UMLClass("PrintForm");
+		List<UMLRelation> entityRelations = createEntityRelations(domainClasses, seamRelations);
 		
-		UMLRelation elementRelation = createAssociation(printEntityClass, printFormClass, "element");
-		classRelations.add(elementRelation);
-		
-		UMLClass stringFieldClass =  new UMLClass("StringField").
-			addAttribute(PRIVATE, "String", "fieldName").
-			addAttribute(PRIVATE, "String", "fieldTitle").
-			addAttribute(PRIVATE, "String", "length");
-		
-		UMLRelation printFormToStringFieldRelation = createAssociation(printFormClass, stringFieldClass, "");
-		classRelations.add(printFormToStringFieldRelation);
-		
-		createAllDomainRelation(printEntityClass, domainClasses, classRelations);
-		
-		domainClasses.add(printDescriptorClass);
-		domainClasses.add(printEntityClass);
-		domainClasses.add(printFormClass);
-		domainClasses.add(stringFieldClass);
+		domainClasses.addAll(seamSupportedClasses);
+		classRelations.addAll(seamRelations);
+		classRelations.addAll(entityRelations);
 	}
 
 	/**
-	 * Iterate over all the domain classes to add an association "entity" between each and the printEntityClass
+	 * Create the list of the classes supported by the seam generation.
+	 * Basically, it's just the classes declared in the seam diagram but without the abstract classes and the classes named Entity.
+	 * 
+	 * @param umlClasses The classes declared in the seam diagram.
+	 * @return
 	 */
-	private void createAllDomainRelation(final UMLClass printEntityClass, final List<UMLClass> domainClasses, final List<UMLRelation> classRelations) {
-		for (UMLClass entityClass : domainClasses) {
-			UMLRelation entityRelation = createAssociation(printEntityClass, entityClass, "entity");
-			classRelations.add(entityRelation);
+	private List<UMLClass> createSeamSupportedClasses(List<UMLClass> umlClasses) {
+		List<UMLClass> seamSupportedClasses = new ArrayList<UMLClass>();
+		
+		for (UMLClass umlClass : umlClasses) {
+			if (! umlClass.isAbstract() && ! umlClass.getName().equalsIgnoreCase(ENTITY_CLASS_NAME)) {
+				seamSupportedClasses.add(umlClass);
+			}
 		}
+		
+		return seamSupportedClasses;
+	}
+
+	/**
+	 * Create the relations between the seam classes and the domain classes.
+	 * A relation between a seam class and a domain class exists when there is an association where the right side
+	 * targets a class named "Entity"
+	 * 
+	 * @param domainClasses
+	 * @param seamRelation
+	 */
+	private List<UMLRelation> createEntityRelations(List<UMLClass> domainClasses, List<UMLRelation> seamRelation) {
+		List<UMLRelation> entityRelations = new ArrayList<UMLRelation>();
+		
+		for (UMLRelation relation : seamRelation) {
+			if (relation.getRightTarget().getName().equalsIgnoreCase(ENTITY_CLASS_NAME)) {
+				UMLClass seamClass = relation.getLeftTarget();
+				for (UMLClass entityClass : domainClasses) {
+					UMLRelation entityRelation = createAssociation(seamClass, entityClass, "entity");
+					entityRelations.add(entityRelation);
+				}
+			}
+		}
+		
+		return entityRelations;
 	}
 }
