1b6d3cf06f6db7b968af459a7a98189692216c4a
raphael.brugier <raphael.brugier@gmail.com>
2010-07-26 14:25:43 +0000

    Refactor the creation of the relations between the classes.

--- GwtGenerator/test/com/objetdirect/gwt/gen/server/gen/TestPrintEntity.java
+++ GwtGenerator/test/com/objetdirect/gwt/gen/server/gen/TestPrintEntity.java
@@ -14,14 +14,20 @@
  */
 package com.objetdirect.gwt.gen.server.gen;
 
+import static com.objetdirect.gwt.gen.server.services.TestGeneratorServiceOneToMany.createUniDirectionalOneToMany;
 import static com.objetdirect.gwt.umlapi.client.umlcomponents.UMLVisibility.PRIVATE;
+import static com.objetdirect.gwt.umlapi.client.umlcomponents.umlrelation.LinkAdornment.NONE;
+import static com.objetdirect.gwt.umlapi.client.umlcomponents.umlrelation.LinkAdornment.WIRE_ARROW;
+import static com.objetdirect.gwt.umlapi.client.umlcomponents.umlrelation.LinkKind.ASSOCIATION_RELATION;
 import static com.objetdirect.seam.TestPrintEntity.testSimpleEntityFaceletText;
 import static com.objetdirect.seam.TestPrintEntity.testSimpleEntityJavaText;
 
 import org.junit.Test;
 
+import com.objetdirect.gwt.gen.server.services.TestGeneratorServiceOneToMany;
 import com.objetdirect.gwt.umlapi.client.umlcomponents.UMLClass;
 import com.objetdirect.gwt.umlapi.client.umlcomponents.UMLObject;
+import com.objetdirect.gwt.umlapi.client.umlcomponents.umlrelation.UMLRelation;
 
 /**
  * @author Raphaël Brugier <raphael dot brugier at gmail dot com>
@@ -63,5 +69,23 @@ public class TestPrintEntity extends TestSeamGenerator {
 		addStringField(printFormInstance, "email", "E-mail", "20");
 		
 		assertGenerated(testSimpleEntityJavaText, testSimpleEntityFaceletText);
+	}
+	
+	@Test
+	public void testFullFeaturedEntity() {
+		UMLClass agencyClass = new UMLClass("Agency").
+			addAttribute(PRIVATE, "String", "name").
+			addAttribute(PRIVATE, "String", "phone").
+			addAttribute(PRIVATE, "String", "email");
+		
+		classes.add(agencyClass);
+		
+		UMLClass employeeClass = new UMLClass("Employee").
+			addAttribute(PRIVATE, "String", "firstName").
+			addAttribute(PRIVATE, "String", "lastName");
+	
+		classes.add(employeeClass);
+		
+		UMLRelation relation = createUniDirectionalOneToMany(agencyClass, employeeClass, "employees");
 	}
 }
